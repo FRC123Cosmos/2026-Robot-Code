0502294dd2c8671eeaade7d0c96855aaca656306
COSMOS-123 <cosmos.first123@gmail.com>
2026-03-17 10:11:32 -0400

    End of Comp 1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -189,7 +189,7 @@ public final class Constants {
         public static final double kIntakeFinalPosition = 112;
 
         public static final double indexerSpeed = 0.8;
-        public static final double intakeSpeed = 0.85; //0.65
+        public static final double intakeSpeed = 0.725; //0.65
         public static final double intakeRollerAgitationSpeed = 0.2;
 
         public static final double indexerPulseSpeed = 0.8;
